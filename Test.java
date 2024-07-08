4b5dbfd8ba13fa97c498c4941a3bbae1d5e705f6
Aasif201 <aasifjamal201@gmail.com>
2024-07-08 12:41:37 +0530

    after setup

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 class Test {
     public static void main(String[] args) {
         System.out.println("Hiii.....");
+
+        System.out.println("checking push operation working or not...");
     }
 }
